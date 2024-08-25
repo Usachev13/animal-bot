48c7d9f6dfa2caaf90a5fce135c4b2410c717a59
Usachev13 <2893318@gmail.com>
2024-08-25 12:09:11 +0300

    начало разработки бота

---
+++ src/test/java/animal_shelter/TgBotApplicationTests.java
@@ -0,0 +1,13 @@
+package animal_shelter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TgBotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
